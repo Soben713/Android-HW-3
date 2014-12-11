5e2dc2f83215d567fde013cd80f7f73fd0517d58
Soheil <user@Soben.local>
2014-12-11 20:27:56 +0330

    Add splash screen files

---
+++ src/com/example/swipetest/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.swipetest;
+
+import android.annotation.SuppressLint;
+import android.app.ActionBar;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+
+import com.viewpagerindicator.CirclePageIndicator;
+
+public class MainActivity extends FragmentActivity {
+	
+	public static String[] studentNumbers = {"90109903", "9010XXXX", "9010YYYY"};
+	AppSectionsPagerAdapter mAppSectionsPagerAdapter;
+    ViewPager mViewPager;
+	
+	@SuppressLint("NewApi")
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		ActionBar actionBar = getActionBar();
+		actionBar.hide();
+		  
+        // Create the adapter that will return a fragment for each of the three primary sections
+        // of the app.
+        mAppSectionsPagerAdapter = new AppSectionsPagerAdapter(getSupportFragmentManager());
+        
+        // Set up the ViewPager, attaching the adapter and setting up a listener for when the
+        // user swipes between sections.
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(mAppSectionsPagerAdapter);
+        mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                // When swiping between different app sections, select the corresponding tab.
+                // We can also use ActionBar.Tab#select() to do this if we have a reference to the
+                // Tab.
+            }
+        });
+        
+        CirclePageIndicator titleIndicator = (CirclePageIndicator)findViewById(R.id.indicator);
+        titleIndicator.setViewPager(mViewPager);
+	}
+	
+	public static class AppSectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public AppSectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+        	Fragment fragment = new SplashFragment();
+        	Bundle args = new Bundle();
+            args.putString(SplashFragment.STUDENT_NUMBER, studentNumbers[i]);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public int getCount() {
+            return studentNumbers.length;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return "Section " + (position + 1);
+        }
+    }
+}
---
+++ src/com/example/swipetest/SplashFragment.java
@@ -0,0 +1,21 @@
+package com.example.swipetest;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class SplashFragment extends Fragment {
+	public static final String STUDENT_NUMBER = "90123456";
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.splash_fragments, container, false);
+        Bundle args = getArguments();
+        ((TextView) rootView.findViewById(R.id.student_number)).setText(args.getString(STUDENT_NUMBER));
+        return rootView;
+    }
+}
