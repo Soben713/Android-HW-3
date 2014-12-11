7f63c68dbcc98c51d6faecfcdefbcd5c6a7e8ee0
Soheil <user@Soben.local>
2014-12-12 02:55:34 +0330

    Implement actionbar mitems

--- src/com/example/androidhw3/MainPageActivity.java
+++ src/com/example/androidhw3/MainPageActivity.java
@@ -1,46 +1,77 @@
 package com.example.androidhw3;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
-
-import com.viewpagerindicator.CirclePageIndicator;
+import android.widget.Toast;
 
 public class MainPageActivity extends FragmentActivity {
 	CalSectionsPagerAdapter calSectionsPagerAdapter;
 	ViewPager mViewPager;
-	public static final int MAX_DAYS = 365*100;
-	int current_page = (int)(MAX_DAYS/2);
+	public static final int MAX_DAYS = 365 * 100;
+	int current_page = (int) (MAX_DAYS / 2);
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu items for use in the action bar
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.main_activity_actions, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
 	
 	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    // Handle presses on the action bar items
+	    switch (item.getItemId()) {
+	        case R.id.action_std1:
+	        	Toast.makeText(getApplicationContext(), "90109903", Toast.LENGTH_SHORT).show();
+	            return true;
+	        case R.id.action_std2:
+	        	Toast.makeText(getApplicationContext(), "9010XXXX", Toast.LENGTH_SHORT).show();
+	            return true;
+	        case R.id.action_std3:
+	        	Toast.makeText(getApplicationContext(), "9010YYYY", Toast.LENGTH_SHORT).show();
+	        	return true;
+	    }
+	    return true;
+	}
+
+	@Override
 	public void onCreate(Bundle savedInstanceState) {
-	    super.onCreate(savedInstanceState);
-	    
-	    setContentView(R.layout.mainpage_activity);
+		super.onCreate(savedInstanceState);
 
-		// Create the adapter that will return a fragment for each of the three primary sections
+		setContentView(R.layout.mainpage_activity);
+
+		// Create the adapter that will return a fragment for each of the three
+		// primary sections
 		// of the app.
-		calSectionsPagerAdapter = new CalSectionsPagerAdapter(getSupportFragmentManager());
+		calSectionsPagerAdapter = new CalSectionsPagerAdapter(
+				getSupportFragmentManager());
 
-		// Set up the ViewPager, attaching the adapter and setting up a listener for when the
+		// Set up the ViewPager, attaching the adapter and setting up a listener
+		// for when the
 		// user swipes between sections.
 		mViewPager = (ViewPager) findViewById(R.id.pager);
 		mViewPager.setAdapter(calSectionsPagerAdapter);
-		mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
-			@Override
-			public void onPageSelected(int position) {
-			}
-		});
+		mViewPager
+				.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+					@Override
+					public void onPageSelected(int position) {
+					}
+				});
 		// Should be changed to current date's integer
 		mViewPager.setCurrentItem(current_page);
 	}
 
-	public static class CalSectionsPagerAdapter extends FragmentStatePagerAdapter {
+	public static class CalSectionsPagerAdapter extends
+			FragmentStatePagerAdapter {
 
 		public CalSectionsPagerAdapter(FragmentManager fm) {
 			super(fm);
@@ -60,13 +91,13 @@ public class MainPageActivity extends FragmentActivity {
 			return MAX_DAYS;
 		}
 	}
-	
-	public void moveToPrevFragment(View view){
+
+	public void moveToPrevFragment(View view) {
 		current_page--;
 		mViewPager.setCurrentItem(current_page);
 	}
-	
-	public void moveToNextFragment(View view){
+
+	public void moveToNextFragment(View view) {
 		current_page++;
 		mViewPager.setCurrentItem(current_page);
 	}
